3d7b67225a066fcd45e6d9ef41ac871c32594089
Emma Pang <emma.kpang@gmail.com>
2020-02-28 22:31:42 -0500

    dropdown appends table now

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -11,23 +11,25 @@
 function buildMetadata(sample) {
   const url = "samples.json";
   // @TODO: Complete the following function that builds the metadata panel
+  
 
   // Use `d3.json` to fetch the metadata for a sample
   d3.json(url).then(function(data) {
+
+    //filter data
+  var dropdownSample = data.metadata.filter(specificSample => specificSample.id == sample);
+
     // Use d3 to select the panel with id of `#sample-metadata`
-    var sampleMetadata = d3.select("sample-metadata");
+    var sampleMetadata = d3.select("#sample-metadata");
       
-    
+    console.log(dropdownSample);
     // // Use `.html("") to clear any existing metadata
     sampleMetadata.html("");
 
-    // // Use `Object.entries` to add each key and value pair to the panel
-    // // Object.entries(data).forEach(([key, value]) => {
-    // //   sampleMetadata.append("h5").text(`${key}:${value}`);
 
-    Object.entries(data).forEach(([key, value]) => {
+    Object.entries(dropdownSample[0]).forEach(([key, value]) => {
       var row = sampleMetadata.append("h5");
-      row.text(`${key}:${value}`);
+      row.text(`${key}: ${value}`);
     });
 
     
@@ -39,29 +41,31 @@ function buildMetadata(sample) {
   });
 }
 
-// function buildCharts(sample) {
+function buildCharts(sample) {
+  const url = "samples.json";
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json(url).then(function(data) {
 
-//   // @TODO: Use `d3.json` to fetch the sample data for the plots
+    var idsamplevalues = data.samples.filter(xvalSample => xvalSample.id == sample);
+    console.log(idsamplevalues);
+    // @TODO: Build horizontal bar chart
 
-//     // @TODO: Build a Bubble Chart using the sample data
-
-//     // @TODO: Build a Pie Chart
-//     // HINT: You will need to use slice() to grab the top 10 sample_values,
-//     // otu_ids, and labels (10 each).
-// }
+    // @TODO: Build a Bubble Chart using the sample data
+    
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+  });
+}
 
 function init() {
-  const url = "samples.json";
-
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
-
   // Use the list of sample names to populate the select options
-  d3.json(url).then((sampleNames) => {
-    var names = sampleNames.names;
-    console.log(names)
-    names.forEach((sample) => {
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
+
+    sampleNames.forEach((sample) => {
       selector
         .append("option")
         .text(sample)
@@ -69,8 +73,7 @@ function init() {
     });
 
     // Use the first sample from the list to build the initial plots
-
-    const firstSample = sampleNames[0];
+    var firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
